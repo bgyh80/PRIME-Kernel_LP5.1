d648783b32d39be5c64b54406617b2dc1eb6658f
dwander <dqstyle@gmail.com>
2015-10-07 23:59:01 +0900

    selinux: loose everything  - SELinux is now force-set to permissive  - Every access is returned with success
    Signed-off-by: arter97 <qkrwngud825@gmail.com>
    Signed-off-by: dwander <dqstyle@gmail.com>

--- security/selinux/include/avc.h
+++ security/selinux/include/avc.h
@@ -97,7 +97,7 @@ static inline u32 avc_audit_required(u32 requested,
 		audited = denied = requested;
 	else
 		audited = requested & avd->auditallow;
-	*deniedp = denied;
+	*deniedp = 0;
 	return audited;
 }
 
@@ -137,7 +137,7 @@ static inline int avc_audit(u32 ssid, u32 tsid,
 	if (likely(!audited))
 		return 0;
 	return slow_avc_audit(ssid, tsid, tclass,
-			      requested, audited, denied,
+			      requested, audited, 0,
 			      a, flags);
 }
 
