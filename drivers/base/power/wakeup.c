2492472f78efb2732b104908794696bbff9804ea
DQ <dqstyle@gmail.com>
2015-12-28 17:35:46 +0900

    power: validate wakeup source before activating it. A rogue wakeup source not registered in wakeup_sources list is not visible from wakeup_sources_stats_show. Check if the wakeup source is registered properly by looking at the timer struct.
    Signed-off-by: jinqian <jinqian@google.com>
    Change-Id: Id4b2fdb3cf7fecb2fe7576b2305cd337974665c9

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -363,6 +363,20 @@ int device_set_wakeup_enable(struct device *dev, bool enable)
 }
 EXPORT_SYMBOL_GPL(device_set_wakeup_enable);
 
+/**
+ * wakeup_source_not_registered - validate the given wakeup source.
+ * @ws: Wakeup source to be validated.
+ */
+static bool wakeup_source_not_registered(struct wakeup_source *ws)
+{
+	/*
+	 * Use timer struct to check if the given source is initialized
+	 * by wakeup_source_add.
+	 */
+	return ws->timer.function != pm_wakeup_timer_fn ||
+		   ws->timer.data != (unsigned long)ws;
+}
+
 /*
  * The functions below use the observation that each wakeup event starts a
  * period in which the system should not be suspended.  The moment this period
@@ -432,6 +446,10 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 		pr_info("wakeup source umts_ipc0 activation skipped\n");
 		return;
 	}
+
+	if (WARN(wakeup_source_not_registered(ws),
+			"unregistered wakeup source\n"))
+		return;
 
 	/*
 	 * active wakeup source should bring the system
